fda3654571bbab3235483783809b4b20a7236c05
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-03-24 15:13:53 -0300

    log(Query Params)

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,38 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+/**
+ * Rotas / Recursos
+ */
+
+/**
+ * Metodos HTTP:
+ *
+ * GET: Buscar/listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de Parametros:
+ *
+ * Query Params: Parametors nomeados enviados na rota apos "?" (filtros, paginação)
+ * Route params: Parametros utilizados para identificar recursos
+ * Request Body: Corpo da requisição
+ *
+ */
+
+
+app.get('/users/', (request, response) => {
+
+	const params = request.query;
+
+	console.log(params);
+
 	return response.json({
 		evento: 'Semana OminiStack 11.0',
-		aluno: 'Tiago Barbosa'
+		aluno: 'Tiago Barbosa',
 	});
 });
 
