4b6a7a2032ea477748552d3b6dd12de8cdd0ed3c
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-03-24 06:26:55 -0300

    alterei retun para json

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-
+	return response.json({
+		evento: 'Semana OminiStack 11.0',
+		aluno: 'Tiago Barbosa'
+	});
 });
 
 app.listen(3333);
