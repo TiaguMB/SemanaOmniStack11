56e570fc432cdc9fe325847f196468d5cf29da4e
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-03-25 10:10:57 -0300

    Request Body

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+/** 
+ * Instalação do nodemom para nao ter que ficar reiniciando o aplicativo
+ * 
+ * npm install nodemom -D
+ * O d serve para que a dependencia seja so executada enquanto ha o desenvolvimento
+ * 
+*/
+
 /**
- * Rotas / Recursos
+ * Rotas = "conjunto completo" | Recursos = "caminho"
  */
 
 /**
@@ -20,16 +30,44 @@ const app = express();
  *
  * Query Params: Parametors nomeados enviados na rota apos "?" (filtros, paginação)
  * Route params: Parametros utilizados para identificar recursos
- * Request Body: Corpo da requisição
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
  *
  */
 
+/**
+ * Exemplo de Query params
+ * Requisição http://localhost:3333/users?name=Matheus&idade=20
+ *
+ * const params = request.query;
+ *
+ * console.log(params)
+ */
 
-app.get('/users/:id', (request, response) => {
+/**
+ * Exemplo de Route params
+ * Requisição http://localhost:3333/users/id
+ * OBS tem que mudar a rota do get
+ *
+ * const params = request.params;
+ *
+ * console.log(params)
+ */
 
-	const params = request.params;
+/**
+ * Exemplo de Request Body
+ *  Requisição http://localhost:3333/users
+ *  OBS Tem que ser um metodo post | Tem que utilizar isso app.use(express.json());
+ * 
+ *  const body = request.body;
+ *
+ * console.log(body)
+ */
 
-	console.log(params);
+app.post('/users', (request, response) => {
+	
+	const body = request.body;
+
+	console.log(body);
 
 	return response.json({
 		evento: 'Semana OminiStack 11.0',
