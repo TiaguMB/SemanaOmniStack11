4d6e03228d50891f0abae9d86fc9b7b647c4000b
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-03-27 11:23:11 -0300

    importei routes no routes.js e criei metodo get, criei connection.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,18 +1,31 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
+
 
 const routes = express.Router();
 
+routes.get('/ongs', async (request, response) => {
+	const ongs = await connection('ongs').select('*');
 
-routes.post('/users', (request, response) => {
-	
-	const body = request.body;
+	return response.json(ongs);
+});
 
-	console.log(body);
+routes.post('/ongs', async (request, response) => {
+	const { name, email, whatsapp, city, uf } = request.body;
 
-	return response.json({
-		evento: 'Semana OminiStack 11.0',
-		aluno: 'Cleitin Rasta',
+	const id = crypto.randomBytes(4).toString('HEX');
+
+	await connection('ongs').insert({
+		id,
+		name,
+		email,
+		whatsapp,
+		city,
+		uf,
 	});
+
+	return response.json({ id });
 });
 
 module.exports = routes;
