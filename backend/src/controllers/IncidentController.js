c0a2a10cdb5146b81c248ac3b80703a1c6dee446
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-03-27 13:32:29 -0300

    Criei rota delete controllers/IncidentController.js

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -19,5 +19,23 @@ module.exports = {
 		});
 
 		return response.json({ id });
+	},
+
+	async delete(request, response) {
+		const { id } = request.params;
+		const ong_id = request.headers.authorization;
+
+		const incident = await connection('incidents')
+			.where('id', id)
+			.select('ong_id')
+			.first();
+		
+		if(incident.ong_id !== ong_id) {
+			return response.status(401).json({ error: 'Operation not permitted.' });
+		}
+
+		await connection('incidents').where('id', id).delete();
+
+		return response.status(204).send();
 	}
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -10,5 +10,6 @@ routes.post('/ongs', OngController.create);
 
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
