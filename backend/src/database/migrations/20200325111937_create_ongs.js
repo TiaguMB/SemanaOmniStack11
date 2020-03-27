d264ea62b172c47ba7c9d2ff14062b99c3fcace8
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-03-27 10:47:11 -0300

    npx knex migrate:make create_incidents

--- backend/src/database/migrations/20200325111937_create_ongs.js
+++ backend/src/database/migrations/20200325111937_create_ongs.js
@@ -1,15 +1,14 @@
-
 exports.up = function(knex) {
-  return knex.schema.createTable('ongs', function(table) {
-	  table.string('id').primary();
-	  table.string('name').notNullable();
-	  table.string('email').notNullable();
-	  table.string('whatasapp').notNullable();
-	  table.string('city').notNullable();
-	  table.string('uf', 2).notNullable();
+	return knex.schema.createTable('ongs', function (table) {
+		table.string('id').primary();
+		table.string('name').notNullable();
+		table.string('email').notNullable();
+		table.string('whatsapp').notNullable();
+		table.string('city').notNullable();
+		table.string('uf', 2).notNullable();
 	});
 };
 
 exports.down = function(knex) {
-  return knes.schema.dropTable('ongs');
+	return knex.schema.dropTable('ongs');
 };
