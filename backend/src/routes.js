6859b0a1df8ff52030abb70d3664a24d232809d0
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-03-27 13:49:13 -0300

    Adicionei ProfileController com casos específicos de uma ONG

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index)
 
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
