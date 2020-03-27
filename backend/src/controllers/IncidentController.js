47529b98d5920306a7d533b3f876f27802e2e6dd
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-03-27 15:29:11 -0300

    Criei paginação de incidents e adicionei API CORS para segurança

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,7 +2,26 @@ const connection = require('../database/connection');
 
 module.exports = {
 	async index(request, response) {
-		const incidents = await connection('incidents').select('*');
+		const { page = 1 } = request.query;
+
+		const [count] = await connection('incidents').count();
+
+		// console.log(count);
+		
+		const incidents = await connection('incidents')
+			.join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+			.limit(5)
+			.offset((page -1) * 5)
+			.select([
+				'incidents.*',
+				'ongs.name',
+				'ongs.email',
+				'ongs.whatsapp',
+				'ongs.city',
+				'ongs.uf'
+			]);
+
+		response.header('X-Total-Count', count['count(*)']);
 
 		return response.json(incidents);
 	},
