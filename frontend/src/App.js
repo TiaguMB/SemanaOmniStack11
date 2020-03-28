2c7520de3eb6ed6e3515ee10ff0b3cc3af4059ad
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-03-28 00:42:53 -0300

    Feito conceitos de Estado e Imutabilidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 
 function App() {
+	const [counter, setCounter] = useState(0);
+	/**
+	 * Array [valor, funcaoDeAtualizacao]
+	 * 
+	 * let counter = 0;
+	 */
+	
+	function increment() {
+		setCounter(counter + 1);
+
+		/**
+		 * counter++;
+		 * console.log(counter);
+		 */
+	}
+
 	return (
-		<Header title="Semana OmniStack" />
+		<div>
+			<Header>Contador: {counter}</Header>
+			<button onClick={increment}>Incrementar</button>
+		</div>
 	);
 }
 
