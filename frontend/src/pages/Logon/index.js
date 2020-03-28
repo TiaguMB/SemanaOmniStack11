d67ceefb8c97e9603644eee660ba4f940cedc168
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-03-28 03:01:43 -0300

    Criei página de login

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi'
 
 import './styles.css';
 
+import heroesImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
+
 export default function Logon() {
 	return(
-		<h1>Hello World</h1>
+		<div className="logon-container">
+			<section className="form">
+				<img src={logoImg} alt="Be The Hero"/>
+
+				<form>
+					<h1>Faça seu logon</h1>
+
+					<input placeholder="Sua ID" />
+					<button className="button" type="submit">Entrar</button>
+
+					<a href="/register">
+						<FiLogIn size={16} color="#e02041" />
+						Não tenho cadastro
+					</a>
+				</form>
+			</section>
+
+			<img src={heroesImg} alt="Heroes"/>
+		</div>
 	);
 }
