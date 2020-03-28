c3f0d3ffa5247b22fc917947ab50374cc0731cda
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-03-28 04:03:24 -0300

    Criei página Register

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi'
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
 
@@ -18,10 +19,10 @@ export default function Logon() {
 					<input placeholder="Sua ID" />
 					<button className="button" type="submit">Entrar</button>
 
-					<a href="/register">
+					<Link className="back-link" to="/register">
 						<FiLogIn size={16} color="#e02041" />
 						Não tenho cadastro
-					</a>
+					</Link>
 				</form>
 			</section>
 
