0c2ec0538137dc527612dcbdc0afc9c3997f458b
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-03-29 18:25:07 -0300

    Criei handleLogout no Profile

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -9,6 +9,8 @@ import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
 	const [incidents, setIncidents] = useState([]);
+
+	const history = useHistory();
 	
 	const ongId = localStorage.getItem('ongId');
 	const ongName = localStorage.getItem('ongName');
@@ -37,6 +39,12 @@ export default function Profile() {
 		}
 	}
 
+	function handleLogout() {
+		localStorage.clear();
+		
+		history.push('/');
+	}
+
 	return (
 		<div className="profile-container">
 			<header>
@@ -44,7 +52,7 @@ export default function Profile() {
 				<span>Bem vinda, {ongName}</span>
 
 				<Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-				<button type="button">
+				<button onClick={handleLogout} type="button">
 					<FiPower size={18} color="#E02041" />
 				</button>
 			</header>
