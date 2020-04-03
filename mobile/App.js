15632c9dceac327b95b16d4a5e8719d99d030573
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-04-03 17:08:06 -0300

    Ícone e Splash Screen

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text style={styles.title}>Hello World</Text>
     </View>
   );
 }
@@ -12,8 +12,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7159c1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  title: {
+    color: '#FFF',
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
 });
