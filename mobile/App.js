d844f1c56416da7996b39a9e47ddbc046be4cc89
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2020-04-04 00:00:02 -0300

    Fiz configuração de navegação e página de casos

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  
-  title: {
-    color: '#FFF',
-    fontSize: 20,
-    fontWeight: 'bold',
-  }
-});
---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native'
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
+
+import logo from '../../assets/logo.png';
+
+import styles from './styles';
+
+export default function() {
+	const navigation = useNavigation();
+
+	function navigatetoDetail() {
+		navigation.navigate('Detail');
+	}
+	
+	return(
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Image source={logo} />
+				<Text style={styles.headerText}>
+					Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+				</Text>
+			</View>
+
+			<Text style={styles.title}>Bem vindo!</Text>
+			<Text style={styles.description}>
+				Escolha um dos casos abaixo e salve o dia.
+			</Text>
+
+			<FlatList
+				data={[1, 2, 3, 4]}
+				style={styles.incidentList}
+				keyExtractor={incident => String(incident)}
+				showsVerticalScrollIndicator={false}
+				renderItem={() => (
+					<View style={styles.incident}>
+						<Text style={styles.incidentProperty}>ONG:</Text>
+						<Text style={styles.incidentValue}>APAD</Text>
+
+						<Text style={styles.incidentProperty}>CASO:</Text>
+						<Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+						
+						<Text style={styles.incidentProperty}>VALOR:</Text>
+						<Text style={styles.incidentValue}>R$ 120,00</Text>
+
+						<TouchableOpacity
+							style={styles.detailsButton}
+							onPress={navigatetoDetail}
+						>
+							<Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+							<Feather name="arrow-right" size={16} color="#E02041" />
+						</TouchableOpacity>
+					</View>
+				)}
+			/>
+				
+			
+		</View>
+	);
+}
